dbd9517a562f2e03453bcf216d681cce337fa7c1
Hien Tran <aznketchup@yahoo.com>
2017-07-28 16:31:14 -0700

    Import script file from Number Wizard. -Adjust code to fit Unity's UI system. -Took out all print and is replaced by changing the text. -Incorporate Random.range(); -Created buttons for the Game scene that tells the application to guess higher, lower, or if the guess is correct.

---
+++ Number Wizard UI/Assets/NumberWizard.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+	public int maxGuessesAllowed = 8;
+	
+	public Text text;
+	
+	void Start () {
+		StartGame ();
+	}
+	
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		guess = Random.Range(min, max);
+		text.text = guess.ToString();
+	}
+	
+	public void GuessHigher(){
+		min = guess + 1;
+		NextGuess();
+	}
+	
+	public void GuessLower(){
+		max = guess - 1;
+		NextGuess();
+	}
+	
+	void NextGuess () {
+		guess = Random.Range (min, max);
+		text.text = guess.ToString();
+		--maxGuessesAllowed;
+		if(maxGuessesAllowed <= 0){
+			Application.LoadLevel("Win");
+		}
+	}
+}
